fc3583a0d91e302f0beca7a7075c9429721846cd
Rodrigo Calábria <rodrigovcalabria@hotmail.com>
2018-04-18 00:01:08 -0300

    Todas as funções implementadas.

--- client.py
+++ client.py
@@ -2,10 +2,43 @@ import xmlrpc.client
 
 s = xmlrpc.client.ServerProxy('http://localhost:8000')
 
-#retorno = s.cadastrar_nota(4, 'calc', 8.5)
-retorno = s.consultar_notas(1)
 
-print(retorno)
+sair = 0
+retorno = "."
+
+while sair != 1:
+	entrada = input("Opções: \n \t1 - Adicionar/alterar nota;\n \t2 - Consultar nota;\n \t3 - Consultar notas;\n \t4 - Consultar CR;\n \t5 - Sair.\n\nEntrada: ")
+
+	if entrada == "1":
+		print("ADICIONAR/ALTERAR NOTA")
+		matricula = input("Matrícula: ")
+		disciplina = input("Disciplina: ")
+		nota = input("Nota: ")
+		retorno = s.cadastrar_nota(int(matricula), disciplina, float(nota))
+
+	elif entrada == "2":
+		print("CONSULTAR NOTA")
+		matricula = input("Matrícula: ")
+		disciplina = input("Disciplina: ")
+		retorno = s.consultar_nota(int(matricula), disciplina)
+
+	elif entrada == "3":
+		print("CONSULTAR NOTAS")
+		matricula = input("Matrícula: ")
+		retorno = s.consultar_notas(int(matricula))
+
+	elif entrada == "4":
+		print("CONSULTAR CR")
+		matricula = input("Matrícula: ")
+		retorno = s.consultar_cr(int(matricula))
+
+	elif entrada == "5":
+		sair = 1
+		retorno = "Tchau."
+
+	print(retorno)
+
+
 
 # Print list of available methods
 #print(s.system.listMethods())
--- server.py
+++ server.py
@@ -1,6 +1,7 @@
 from xmlrpc.server import SimpleXMLRPCServer
 from xmlrpc.server import SimpleXMLRPCRequestHandler
 from pymongo import MongoClient
+import numpy as np
 
 # Restrict to a particular path.
 class RequestHandler(SimpleXMLRPCRequestHandler):
@@ -37,23 +38,45 @@ def cadastrar_nota (mat, cod_disc, nota):
 server.register_function(cadastrar_nota, 'cadastrar_nota')
 
 
-#def consultar_nota (mat, cod_disc):
+def consultar_nota (mat, cod_disc):
+	registro = boletim_collection.find_one({"matricula": mat, "disciplina": cod_disc})
+	if registro != None:
+		return registro['nota']
+	else:
+		return "Não há notas cadastradas para este aluno nesta disciplina."
+
+server.register_function(consultar_nota, 'consultar_nota')
 
 
 def consultar_notas (mat):
 	lista_notas = []
 	lista_registros = boletim_collection.find({"matricula": mat})
-	if lista_registros != None:
+	if lista_registros.count() != 0:
 		for registro in lista_registros:
 			nota = registro['nota']
 			disciplina = registro['disciplina']
 			lista_notas.append([nota, disciplina])
 
-	return lista_notas
+		return lista_notas
+	else:
+		return "Não há notas cadastradas para este aluno."
 
 server.register_function(consultar_notas, 'consultar_notas')
 
-#def consultar_cr (mat):
+def consultar_cr (mat):
+	lista_notas = []
+	lista_registros = boletim_collection.find({"matricula": mat})
+	if lista_registros.count() != 0:
+		for registro in lista_registros:
+			lista_notas.append(registro['nota'])
+
+		valor_retorno = np.mean(lista_notas).astype(type('float', (float,), {}))
+	else:
+		valor_retorno = "Não há notas registradas para este aluno."
+
+	return valor_retorno
+
+server.register_function(consultar_cr, 'consultar_cr')
 
 print("rodando.")
 
